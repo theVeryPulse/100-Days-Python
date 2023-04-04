15c51b67254cd263ae0e8e1cf053e430c9c9c5a6
Philip Li <125798752+TheVeryPulse@users.noreply.github.com>
2023-04-04 14:45:38 +0100

    Add files via upload

---
+++ 2023.04.0.4_finance_calculators.py
@@ -0,0 +1,48 @@
+# Purpose: To calculate the amount of interest you'll earn on your investment
+#          or the amount you'll have to pay on a home loan
+
+import math
+
+print("""
+investment - to calculate the amount of interest you'll earn on your investment
+bond       - to calculate the amount you'll have to pay on a home loan
+""")
+
+# Either investment or bond?
+type = ""
+type = input("Enter either 'investment' or 'bond' from the menu above to proceed: ").lower()
+while type != "investment" and type != "bond":
+    type = input("Sorry I don't understand. Please enter 'investment' or 'bond': ").lower()
+
+
+if type == "investment":
+    # Ask for basic investment information
+    deposit = float(input("How much are you depositing?\n"))
+    interest_rate = float(input("What is the interest rate? (Enter the number. For example, enter '8' for 8%))\n"))
+    interest_rate = interest_rate / 100
+    years = int(input("How many years do you plan on investing?\n"))
+
+    # Ask investment type
+    interest = input("Do you want simple or compound interest? (Enter 'simple' or 'compound')\n").lower()
+    while interest != "simple" and interest != "compound":
+        interest = input("Sorry I don't understand. Please enter 'simple' or 'compound':\n").lower()
+    
+    # Calculate and display the interest
+    if interest == "simple":
+        total_amount = deposit * (1 + interest_rate * years)
+        print("You will earn R{:.2f} in interest.".format(total_amount - deposit))
+    else:
+        total_amount = deposit * math.pow((1 + interest_rate), years)
+        print("You will earn R{:.2f} in interest.".format(total_amount - deposit))
+
+# type == "bond"
+else:
+    # Ask for basic bond information
+    house_value = float(input("What is the present value of the house?\n"))
+    interest_rate = float(input("What is the interest rate? (Enter the number. For example, enter '8' for 8%))\n"))
+    interest_rate = interest_rate / 100
+    months = int(input("How many months do you plan to take to repay the bond?\n"))
+
+    # Calculate and display the repayment
+    repayment = (interest_rate * house_value) / (1 - (1 + interest_rate) ** (- months))
+    print("You will need to pay R{:.2f} each month.".format(repayment))

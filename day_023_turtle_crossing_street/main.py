41e8e768f585cfee842214d5601fbb54c4d31726
Philip Li <macpilot23@outlook.com>
2023-05-14 12:40:13 +0100

    2023.05.14 turtle crossing road game

--- day_023_turtle_crossing_street/main.py
+++ day_023_turtle_crossing_street/main.py
@@ -1,14 +1,73 @@
+# this is a turtle crossing road game
+# cars are randomly spawned on the right hand side and moves leftwards
+# press 'Up' key to move turtle to finish line to win and enter next level
+# when the turtle gets hit by car, the game ends
+# skills: class inheritance, class, Turtle
+
 import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 600
+GAME_SPEED = 0.01    # the smaller the value the quicker
+CAR_HIT_BOX_LEN = 20
+CAR_HIT_BOX_WID = 10
+TURTLE_HIT_BOX_RAD = 10
+
+
+def collision():
+    print(car_manager.cars)
+    for car in car_manager.cars:
+        print(F'car number: {car_manager.cars.index(car)}')
+        x_distance = abs(car.xcor() - player.xcor())
+        y_distance = abs(car.ycor() - player.ycor())
+        print(F'x_distance: {x_distance}, y_distance: {y_distance}')
+        if x_distance < CAR_HIT_BOX_LEN + TURTLE_HIT_BOX_RAD:
+            if y_distance < CAR_HIT_BOX_WID + TURTLE_HIT_BOX_RAD:
+                print('Collision!')
+                return True
+            else:
+                print('no collision 1')
+        else:
+            print('no collision 2')
+    return False
+
+
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=600, height=WINDOW_HEIGHT)
 screen.tracer(0)
+screen.listen()
+
+player = Player()
+screen.onkeypress(player.move, 'Up')
+
+scoreboard = Scoreboard()
+car_manager = CarManager()
+
+# generate a number of cars on map before game begins
+for i in range(2000):
+    car_manager.spawn_cars(WINDOW_WIDTH)
+    car_manager.cars_move()
+    car_manager.remove_outside_cars(WINDOW_WIDTH)
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(GAME_SPEED)
+    car_manager.spawn_cars(WINDOW_WIDTH)
+    car_manager.cars_move()
+    # car_manager.car_number_check()
+    car_manager.remove_outside_cars(WINDOW_WIDTH)
     screen.update()
+    if player.reach_finish_line():
+        car_manager.next_level()
+        scoreboard.next_level()
+        player.reset_turtle()
+        time.sleep(1)
+
+    if collision():
+        game_is_on = False
+
+screen.exitonclick()
--- day_023_turtle_crossing_street/player.py
+++ day_023_turtle_crossing_street/player.py
@@ -1,7 +1,33 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
-MOVE_DISTANCE = 10
+MOVE_DISTANCE = 5
 FINISH_LINE_Y = 280
+FACING_NORTH = 90
 
 
-class Player:
-    pass
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.setheading(FACING_NORTH)
+        self.setpos(STARTING_POSITION)
+        self.shape('turtle')
+
+    def move(self):
+        """move the turtle upwards, return True when reaching finish line"""
+        self.forward(MOVE_DISTANCE)
+
+    def reach_finish_line(self):
+        if self.ycor() >= FINISH_LINE_Y:
+            return True
+
+    def reset_turtle(self):
+        self.hideturtle()
+        self.setpos(STARTING_POSITION)
+        self.showturtle()
+
+
+
+

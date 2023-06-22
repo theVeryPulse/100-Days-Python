e858241eb1c7232d1633c95ebcbdeaa8a18331a0
Philip Li <macpilot23@outlook.com>
2023-06-22 17:05:01 +0100

    2023.06.22 format improve

--- day_025_states_guess_game/pandas_practice_code/pandas_practice_code.py
+++ day_025_states_guess_game/pandas_practice_code/pandas_practice_code.py
@@ -6,13 +6,13 @@ data = pd.read_csv('weather_data.csv')    # read csv with a panda method
 
 temp_list = data['temp'].to_list()    # cast the Series of 'temp' to a list
 
-print(data['temp'].mean(0))    # calculate mean value using a pandas method on a series object
+print(data['temp'].mean(0).round(3))   # calculate mean value using a pandas method on a series object
 
 # find the day with the highest temperature
-print('Day with the highest temperature', data[data.temp == data.temp.max()])
+print('\nDay with the highest temperature: \n', data[data.temp == data.temp.max()])
 
 monday = data[data.day == 'Monday']    # extract a row rather than a column of the DataFrame
-print(monday)
+print('\nMonday: \n', monday)
 
 monday_temp = int(monday.temp.iloc[0])    # extract the value of an attribute
 monday_temp_F = (monday_temp * 9 / 5) + 32    # conversion of Celsius into Fahrenheit

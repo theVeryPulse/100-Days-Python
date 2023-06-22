41c57d21a8cbd95b7e94692775b485d27b15d311
Philip Li <macpilot23@outlook.com>
2023-06-22 17:50:33 +0100

    2023.06.22

---
+++ day_026/NATO-alphabet/main.py
@@ -0,0 +1,28 @@
+# This program takes words and converts them into their corresponding nato codes
+# SKILLS: dictionary comprehension, pandas
+
+import pandas as pd
+# pandas method: iterrows(), returns (row_index, row_content)
+
+# Create a dictionary in this format: {"A": "Alfa", "B": "Bravo" ... "Z": "Zulu"}
+content = pd.read_csv('nato_phonetic_alphabet.csv')
+nato_ab = {row.letter: row.code for (index, row) in content.iterrows()}
+# print(nato_ab)
+
+# Create a list of the phonetic code words from words that users input.
+while True:
+    word = input("What word would you like to convert?\n")
+    converted = []
+    for char in word:
+        try:
+            char_code = nato_ab[char.upper()]
+        except Exception:
+            converted.append(char)
+        else:
+            converted.append(char_code)
+    print(converted)
+
+
+
+
+
--- day_026/list_comprehension_practice.py
+++ day_026/list_comprehension_practice.py
@@ -1,10 +1,34 @@
+# list comprehension examples
+# each elements + 1
 a_list = [1, 2, 3]
 b_list = [n + 1 for n in a_list[::-1]]
 print(b_list)
 
+# each element * 2
 c_list = [2 * n for n in range(1, 4)]
 print(c_list)
 
+# each element to upper case
 d_list = ['Alex', 'Beth', 'Caroline']
 e_list = [name.upper() for name in d_list]
 
+
+# dictionary comprehension example
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+s_list = sentence.split()
+result = {word: len(word) for word in s_list}
+print(result)
+
+
+# convert celsius to fahrenheit
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+weather_f = {day: (temp_c * 9/5) + 32 for (day, temp_c) in weather_c.items()}  # use .items() to get both key and value
+print(weather_f)

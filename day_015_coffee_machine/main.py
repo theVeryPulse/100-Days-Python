6ed00e8e6c7220d4163e8edc9bd187b49bc39b20
Philip Li <macpilot23@outlook.com>
2023-07-09 16:05:26 +0100

    2023.07.09

--- day_015_coffee_machine/main.py
+++ day_015_coffee_machine/main.py
@@ -36,16 +36,13 @@ resources = {
 
 profit = 0
 
-# TODO-1
 # 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
 #   a. Check the user’s input to decide what to do next.
 #   b. The prompt should show every time action has completed, e.g. once the drink is
 #       dispensed. The prompt should show again to serve the next customer.
-# Todo-2
 # 2. Turn off the Coffee Machine by entering “off” to the prompt.
 #   a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off
 #       the machine. Your code should end execution when this happens.
-# Todo-3
 # 3. Print report.
 #   a. When the user enters “report” to the prompt, a report should be generated that shows
 #       the current resource values. e.g.
@@ -53,21 +50,18 @@ profit = 0
 #       Milk: 50ml
 #       Coffee: 76g
 #       Money: $2.5
-# Todo-4
 # 4. Check resources sufficient?
 #   a. When the user chooses a drink, the program should check if there are enough
 #       resources to make that drink.
 #   b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
 #       not continue to make the drink but print: “Sorry there is not enough water.”
 #   c. The same should happen if another resource is depleted, e.g. milk or coffee.
-# todo-5
 # 5. Process coins.
 #   a. If there are sufficient resources to make the drink selected, then the program should
 #       prompt the user to insert coins.
 #   b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
 #   c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
 #       pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
-# todo-6
 # 6. Check transaction successful?
 #   a. Check that the user has inserted enough money to purchase the drink they selected.
 #       E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the
@@ -81,7 +75,6 @@ profit = 0
 #   c. If the user has inserted too much money, the machine should offer change.
 #       E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal
 #        places.
-# todo-7
 # 7. Make Coffee.
 #   a. If the transaction is successful and there are enough resources to make the drink the
 #       user selected, then the ingredients to make the drink should be deducted from the
